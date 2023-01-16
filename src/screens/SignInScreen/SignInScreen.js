292655a501daf9e70f44746be827dae31243f4c2
Creano_yanis <yanis@creano.paris>
2023-01-16 22:34:06 +0100

    ajout des sécurité sur les champs

--- src/screens/SignInScreen/SignInScreen.js
+++ src/screens/SignInScreen/SignInScreen.js
@@ -12,15 +12,20 @@ import CustomInput from '../../components/CustomInput';
 import CustomButton from '../../components/CustomButton';
 import SocialSignInButtons from '../../components/SocialSignInButtons';
 import {useNavigation} from '@react-navigation/native';
+import {useForm, Controller} from 'react-hook-form';
 
 const SignInScreen = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-
   const {height} = useWindowDimensions();
   const navigation = useNavigation();
 
-  const onSignInPressed = () => {
+  const {
+    control,
+    handleSubmit,
+    formState: {errors},
+  } = useForm();
+
+  const onSignInPressed = data => {
+    console.log(data);
     // validate user
     navigation.navigate('Home');
   };
@@ -33,6 +38,7 @@ const SignInScreen = () => {
     navigation.navigate('SignUp');
   };
 
+
   return (
     <ScrollView showsVerticalScrollIndicator={false}>
       <View style={styles.root}>
@@ -43,15 +49,24 @@ const SignInScreen = () => {
         />
         <Text>GEOTRACK</Text>
         <CustomInput
+          name="username"
           placeholder="Username"
-          value={username}
-          setValue={setUsername}
+          control={control}
+          rules={{required: 'Username is required'}}
         />
+        
         <CustomInput
+          name="password"
           placeholder="Password"
-          value={password}
-          setValue={setPassword}
           secureTextEntry
+          control={control}
+          rules={{
+            required: 'Password is required',
+            minLength: {
+              value: 3,
+              message: 'Password should be minimum 3 characters long',
+            },
+          }}
         />
 
         <CustomButton text="Sign In" onPress={onSignInPressed} />
