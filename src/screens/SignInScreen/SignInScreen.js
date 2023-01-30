e48558663019c5622540681b6853bdcba9477e9c
Creano_yanis <yanis@creano.paris>
2023-01-30 01:48:30 +0100

    trying to add Facebook login

--- src/screens/SignInScreen/SignInScreen.js
+++ src/screens/SignInScreen/SignInScreen.js
@@ -13,10 +13,14 @@ import CustomButton from '../../components/CustomButton';
 import SocialSignInButtons from '../../components/SocialSignInButtons';
 import {useNavigation} from '@react-navigation/native';
 import {auth} from '../../../firebase';
+import * as Facebook from 'expo-facebook';
+import * as Location from 'expo-location';
 
 const SignInScreen = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [location, setLocation] = useState({});
+
 
   const {height} = useWindowDimensions();
   const navigation = useNavigation();
@@ -27,8 +31,83 @@ const SignInScreen = () => {
           navigation.replace("Home");
       }
   })
+  getLocation();
+
+
   },[])
 
+  const handleFacebookLogin = async () => {
+
+    try {
+      await Facebook.initializeAsync({  
+        appId: '1649158192208792',
+      });
+      const { type, token } = await Facebook.logInWithReadPermissionsAsync(
+        { permissions: ['public_profile', 'email'] }
+      );
+      if (type === 'success') {
+        // Get the user's name and email using Facebook's Graph API
+        const response = await axios.get(`https://graph.facebook.com/me?access_token=${token}&fields=id,name,email`);
+        const user = response.data;
+        console.log(`Logged in with ${user.name}`);
+        
+        // Create a credential with the token
+        const credential = firebase.auth.FacebookAuthProvider.credential(token);
+        
+        // Sign in with the credential
+        auth.signInWithCredential(credential)
+            .then(userCredentials => {
+              const user = userCredentials.user;
+              console.log(`Logged in with ${user.email}`);
+
+              const bodyUser = {
+                id: user.uid,
+                email: user.email,
+                name: user.name,
+                freinds: []
+              }
+              const bodyLocation = {
+                id: user.uid,
+                lat: location?.coords.latitude,
+                long: location?.coords.longitude
+              };
+              pushData(bodyUser);
+              pushLocation(bodyLocation);
+            })
+            .catch(err => alert(err.message));
+      } else {
+        console.log('Cancelled login');
+      }
+    } catch (err) {
+      console.log('Error logging in', err);
+    }
+  };
+
+  const pushData = async (body) => {
+    const response = await axios.post("https://geoapi.azurewebsites.net/user", body);
+    console.log(response);        
+  }
+
+  const pushLocation = async (body) => {
+    const response = await axios.post("https://geoapi.azurewebsites.net/location", body);
+    console.log(response);
+  }
+  console.log("location is ", location);
+  const getLocation = async () =>{
+    try {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+      const location = await Location.getCurrentPositionAsync({
+        accuracy: Location.Accuracy.High,
+      });
+      setLocation(location);
+    } catch (error) {
+      console.log(error);
+  }
+}
   const handleLogin = () =>{
     auth.signInWithEmailAndPassword(email, password)
         .then(userCredntials =>{
@@ -75,7 +154,28 @@ const SignInScreen = () => {
           type="TERTIARY"
         />
 
-        <SocialSignInButtons />
+        <CustomButton
+        text="Sign In with Facebook"
+        // onPress={handleFacebookLogin}
+        bgColor="#E7EAF4"
+        fgColor="#4765A9"
+        image = {require('../../../assets/images/facebook.png')}
+      />
+      <CustomButton
+        text="Sign In with Google"
+        // onPress={onSignInGoogle}
+        bgColor="#FAE9EA"
+        fgColor="#DD4D44"
+        image = {require('../../../assets/images/google.png')}
+      />
+      <CustomButton
+        text="Sign In with Apple"
+        // onPress={onSignInApple}
+        bgColor="#e3e3e3"
+        fgColor="#363636"
+        image = {require('../../../assets/images/apple.png')}
+
+      />
 
         <CustomButton
           text="Don't have an account? Create one"
